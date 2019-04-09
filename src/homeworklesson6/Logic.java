d6f5d9ac5db5b109e312021c0180efabd44affc7
olena2013 <32085254+olena2013@users.noreply.github.com>
2019-04-09 06:54:31 -0700

    homework 7

---
+++ src/homeworklesson6/Logic.java
@@ -0,0 +1,17 @@
+package homeworklesson6;
+
+public class Logic {
+
+  public void sumDouble(double a, double b){
+    double c = a + b ;
+    System.out.println( c );
+  }
+  public void compareString(String one, String two){
+    if(one.contains(two)){
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
+  }
+
+}
---
+++ src/homeworklesson6/Main.java
@@ -0,0 +1,18 @@
+package homeworklesson6;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Logic novyiObject = new Logic();
+
+    novyiObject.sumDouble(34.67,76.93);
+    novyiObject.sumDouble(6.77,95.2);
+    novyiObject.sumDouble(5.0,22.45);
+
+    novyiObject.compareString("java","javascript");
+    novyiObject.compareString("hello","hi");
+    novyiObject.compareString("Spring","Spring");
+
+
+  }
+}
