5b2259c43e920853db3ebecced962148987cf1f7
olena2013 <32085254+olena2013@users.noreply.github.com>
2019-04-24 08:01:48 -0700

    homework 10 add

---
+++ src/homeworklesson20/Main.java
@@ -0,0 +1,14 @@
+package homeworklesson20;
+
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+
+  int[] x = SortMethod.sort(int[8,2,5]);
+    System.out.println(x);
+
+    }
+
+  }
+
---
+++ src/homeworklesson20/SortMethod.java
@@ -0,0 +1,17 @@
+package homeworklesson20;
+
+
+import java.util.Arrays;
+
+public class SortMethod {
+
+  public static int[] sort(int[] arr) {
+    int[] a = new int[];
+    for(int i = 0; i < a.length; i++){
+      System.out.println(" " + i);
+    }
+    Arrays.sort(a);
+
+  }
+
+}
